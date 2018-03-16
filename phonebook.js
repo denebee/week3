59266a28b89405cae58038282de2d21ad2fcdedf
denebee <37203294+denebee@users.noreply.github.com>
2018-03-16 16:18:15 +0900

    Update phonebook.js

--- phonebook.js
+++ phonebook.js
@@ -5,5 +5,22 @@ document.getElementById("btnSave").onclick=function(){
     phoneNumber = document.getElementById("pn").value;
 
 
-    alert(lastName+firstName+phoneNumber);
+    //alert(lastName+firstName+phoneNumber);
+    table=document.getElementById("mytable");
+    tr=document.createElement("tr");
+    td1=document.createElement("td");
+    td2=document.createElement("td");
+    td3=document.createElement("td");
+    td1.innerText=lastName;
+    td2.innerText=firstName;
+    td3.innerText=phoneNumber;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+   
+   /* tr.innerHTML="<td>"+lastName+"</td>";
+    tr.innerHTML="<td>"+firstName+"</td>";
+    tr.innerHTML="<td>"+phoneNumber+"</td>";
+    */
+    table.appendChild(tr);
 }
